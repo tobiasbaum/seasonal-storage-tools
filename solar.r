a96711d85d6327ef016f0d6dfae3a8aab1b7b63c
Tobias Baum <tobias.baum@set.de>
2026-01-10 23:39:13 +0100

    weiter aufgeräumt

--- solar.r
+++ solar.r
@@ -1,11 +1,10 @@
 csvRaw <- read.csv("solar.csv", sep=";", na.strings = "NaN")
 
-# remove outliers
 csv <- csvRaw
 csv$production <- NULL
-# set outliers to NA
-csv <- csv[csv$solarProduction < 30,]
-csv <- csv[csv$consumption < 90,]
-csv <- csv[csv$export < csv$solarProduction + 1,]
+# remove outliers
+csv <- csv[is.na(csv$solarProduction) | csv$solarProduction < 30,]
+csv <- csv[is.na(csv$consumption) | csv$consumption < 90,]
+csv <- csv[is.na(csv$export) | is.na(csv$solarProduction) | csv$export < csv$solarProduction + 1,]
 
 write.csv(csv, 'filtered_data.csv')
